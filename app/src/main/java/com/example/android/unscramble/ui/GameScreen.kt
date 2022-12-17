96c3bcbd42c529c6deadf3e70af4f4cc1b6d2cd4
mata <eloy.mata@unirioja.es>
2022-12-17 19:09:11 +0100

    Codelab completado

--- app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
+++ app/src/main/java/com/example/android/unscramble/ui/GameScreen.kt
@@ -98,6 +98,11 @@ fun GameScreen(
                 Text(stringResource(R.string.submit))
             }
         }
+        if (gameUiState.isGameOver) {
+            FinalScoreDialog(
+                score = gameUiState.score,
+                onPlayAgain = { gameViewModel.resetGame() })
+        }
     }
 }
 
@@ -174,6 +179,7 @@ fun GameLayout(
  */
 @Composable
 private fun FinalScoreDialog(
+    score: Int,
     onPlayAgain: () -> Unit,
     modifier: Modifier = Modifier
 ) {
@@ -186,7 +192,7 @@ private fun FinalScoreDialog(
             // onCloseRequest.
         },
         title = { Text(stringResource(R.string.congratulations)) },
-        text = { Text(stringResource(R.string.you_scored, 0)) },
+        text = { Text(stringResource(R.string.you_scored, score)) },
         modifier = modifier,
         dismissButton = {
             TextButton(
